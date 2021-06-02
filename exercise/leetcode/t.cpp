c2c42daed3316f839eb32df8c537713ca53ef74f
dudu <tangduguang@gmail.com>
2021-06-02 21:24:21 +0800

    新问题

--- exercise/leetcode/t.cpp
+++ exercise/leetcode/t.cpp
@@ -1,55 +1,18 @@
-之字形层次遍历，一层从左往右， 下一层从右往左， 依次循环
-
-可以来一遍普通的， 再依次的去反转列表中的双数元素。 这应该不是想要的解法。
-
-
-考虑递归的方式， 由于已经有层号， 如果是偶数层就前插， 而不是push_back
-
-
-
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    int lengthOfLongestSubstring(string s) {
+    
+    int ans=0, left=-1;
+    unordered_map<int, int> m;
 
-        std::vector<std::vector<int> > v;
-        if(root == nullptr) return v;
-
-        subLevelOrder(root, 0, v);
-        return v;
-    }
-
-    void subLevelOrder(TreeNode* cur, int lvl, std::vector<std::vector<int> > &v){
-
-        if(lvl>=v.size()){
-            std::vector<int> t = {};
-            v.push_back(t);
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if(m.count(s[i])>0&&m[s[i]]>left){
+            left = m[s[i]];
         }
-        std::vector<int> t = v[lvl];
-        if(lvl %2 !=0)
-            t.push_back(cur->val);
-        else
-            t.insert(t.begin(), cur->val);
-
-        v[lvl] = t;
-
-        if(cur->left!=nullptr)
-            subLevelOrder(cur->left, lvl+1, v);
-
-        if(cur->right!=nullptr)
-            subLevelOrder(cur->right, lvl+1, v);
-        
+        m[s[i]]=i;
+        ans = max(ans, i-left);
+    }
+    return ans;
     }
 };
--- exercise/leetcode/字符串/Longest Substring Without Repeating Characters.md
+++ exercise/leetcode/字符串/Longest Substring Without Repeating Characters.md
@@ -2,7 +2,9 @@
 
 Given a string s, find the length of the longest substring without repeating characters.
 
-题目比较简单, 
+题目比较简单
+
+从后往前的解法
 
 使用一个辅助的数组 a, 记录该位置字符开始的最长的无重复子串长度。a[len-1] = 1 
 用从右往左的方式遍历，int i = len - 2,  假设当前遍历到 i, 则从i+1开始, 从左右往依次检查， 由于前一轮已经计算得到 a[i+1], 这个数字表示以i+1开始的字符，的最长无重复重复的子串的长度， 所以一轮检查的右边界就是 last = i+a[i+1]  
@@ -32,5 +34,35 @@ public:
     		max = t>max?t:max; 
     	}
     	return max;
+    }
+};
+
+从前往后 滑动窗口的解法
+
+维护一个窗口， 记录当面扫描到的无重复字符的子串
+
+i = 0->n-1, 从左到右扫描
+left 为窗口的左边界-1， left = -1
+
+res 为累计的最长长度。
+
+使用一个map , 记录当前字符s[i]上一次出现的位置。 如果上次已经出现而且在窗口内， 则窗口需要调整为 map[s[i]]
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    
+    int ans=0, left=-1;
+    unordered_map<int, int> m;
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if(m.count(s[i])>0&&m[s[i]]>left){
+            left = m[s[i]];
+        }
+        m[s[i]]=i;
+        ans = max(ans, i-left);
+    }
+    	return ans;
     }
 };
